aced0984dc37f15826971b82e1197d3ceba12ef0
cadumancini <cadumancini@gmail.com>
2020-01-03 02:31:41 -0300

    Criação do projeto Bandanas

---
+++ Bandanas/Controllers/CategoriasController.cs
@@ -0,0 +1,53 @@
+﻿using Bandanas.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Bandanas.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private static IList<Categoria> categorias = new List<Categoria>()
+        {
+            new Categoria()
+            {
+                CategoriaId = 1,
+                Nome = "Comemorativas"
+            },
+            new Categoria()
+            {
+                CategoriaId = 2,
+                Nome = "Floresta"
+            },
+            new Categoria()
+            {
+                CategoriaId = 3,
+                Nome = "Animais"
+            },
+            new Categoria()
+            {
+                CategoriaId = 4,
+                Nome = "Objetos"
+            },
+            new Categoria()
+            {
+                CategoriaId = 5,
+                Nome = "Filmes"
+            },
+        };
+
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Categoria categoria)
+        {
+            categorias.Add(categoria);
+            categoria.CategoriaId = categorias.Select(m => m.CategoriaId).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
